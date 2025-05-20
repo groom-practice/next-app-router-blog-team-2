7c77d95ab3a0ab73649e9cdad43b42b9d2496eae
김소연 <ksy9252@naver.com>
2025-05-20 10:25:22 +0900

    Feat : 공통 레이아웃, Link 추가

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { inter } from "@/app/ui/fonts";
 
 export const metadata = {
   title: "블로그",
@@ -10,11 +10,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Link>HOME</Link>
-        <Link href>글 작성</Link>
+      <body className={`${inter.className} antialiased`}>
+        <header className="m-4">
+          <nav className="space-x-3">
+            <Link href="/">HOME</Link>
+            <Link href="/write">글 작성</Link>
+          </nav>
+        </header>
+        {children}
       </body>
     </html>
   );
