3ba6fd677f16a88eb09cf96e264634d5480761b3
zero <inzero1998@gmail.com>
2025-05-20 10:57:02 +0900

    feat: 상세페이지, 404

---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+// app/not-found.js
+export default function NotFoundPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-red-600">
+        404 - 페이지를 찾을 수 없습니다
+      </h1>
+    </div>
+  );
+}
